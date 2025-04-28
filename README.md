e96f966784a2d2d18f7f837f46c4d4257ea02419
Aarnav Pai <52203828+arnu515@users.noreply.github.com>
2025-04-29 01:33:32 +0530

    feat: http config

---
+++ README.md
@@ -0,0 +1,48 @@
+# HTTP and Socks5 Proxy
+
+This is a simple proxying service written in Rust.
+
+It is powered by [`fast-socks5`](https://lib.rs/fast-socks5) and [`axum`](https://lib.rs/axum).
+
+## What it is
+
+- A SOCKS5 proxy.
+- A HTTP proxy (coming soon) with a web frontend.
+  - Supports HTTP/1 and HTTP/2.
+  - HTTP/3 is unfortunately not supported, at least until hyper + axum supports it.
+- Supports authentication via LDAP.
+
+## Configuration
+
+The service is configured using environment variables.
+
+| Name | Description | Default |
+| ---- | ----------- | ------- |
+| `REVPROXY_SOCKS_HOST` | The binding hostname | `127.0.0.1` |
+| `REVPROXY_SOCKS_PORT` | The binding port | `1080` |
+| `REVPROXY_HTTP_HOST` | The binding hostname | `127.0.0.1` |
+| `REVPROXY_HTTP_PORT` | The binding port | `8080` |
+| `REVPROXY_HTTPS` | To enable https, set to `1` | _https disabled_ (`0`) |
+| `REVPROXY_HTTPS_HOST` | The binding hostname | `127.0.0.1` |
+| `REVPROXY_HTTPS_PORT` | The binding port | `4430` |
+| `REVPROXY_HTTPS_CERT` | Path to HTTPS cert | _required_ |
+| `REVPROXY_HTTPS_CERT_KEY` | Path to HTTPS cert key | _required_ |
+| `REVPROXY_PUB_ADDR` | Proxy public IP, will be the reply address | _optional_ |
+| `REVPROXY_{HTTP,SOCKS}_AUTH_METHOD` | Authentication method. See [authentication](#authentication) | `userpass` |
+| `REVPROXY_{HTTP,SOCKS}_AUTH_USER` | Username for `userpass` auth | `user` |
+| `REVPROXY_{HTTP,SOCKS}_AUTH_PASS` | Password for `userpass` auth | `pass` |
+| `REVPROXY_SOCKS_TIMEOUT` | Timeout (sec) (setting to 0 does not disable!) | `10` |
+| `REVPROXY_SOCKS_ENABLE_UDP` | To enable proxying UDP connections via SOCKS5, set to `1` | _disabled_ (`0`) |
+| `REVPROXY_SOCKS_RESOLVE_DNS` | To disolve resolving DNS in SOCKS5, set to `0` | _enabled_ (`1`) |
+
+Notes:
+
+- If HTTPS is enabled, the CERT and CERT_KEY paths must be set.
+
+## Authentication
+
+There are three available modes:
+
+- `no_auth` -- let anyone through.
+- `userpass` -- let some user and password through.
+- `ldap` -- coming soon
--- src/config.rs
+++ src/config.rs
@@ -7,6 +7,11 @@ pub enum Auth {
 }
 
 pub struct Config {
+    socks5: SocksConfig,
+    http: HttpConfig,
+}
+
+pub struct SocksConfig {
     pub addr: String,
     /// the revproxy's IP address -- to be sent
     /// in reply packets. (required for UDP)
@@ -17,60 +22,33 @@ pub struct Config {
     pub enable_udp: bool,
 }
 
-impl Config {
-    #[inline]
-    fn get_env(key: &str, def: String) -> String {
-        match std::env::var(key) {
-            Ok(var) => var,
-            Err(std::env::VarError::NotPresent) => def,
-            Err(_) => panic!("Env var {key} is not unicode"),
-        }
-    }
-
-    #[inline]
-    fn get_env_opt(key: &str) -> Option<String> {
-        match std::env::var(key) {
-            Ok(var) => Some(var),
-            Err(std::env::VarError::NotPresent) => None,
-            Err(_) => panic!("Env var {key} is not unicode"),
-        }
-    }
-
-    #[inline]
-    fn get_env_bool(key: &str) -> Option<bool> {
-        match std::env::var(key) {
-            Ok(var) => Some(&var != "0"),
-            Err(std::env::VarError::NotPresent) => None,
-            Err(_) => panic!("Env var {key} is not unicode"),
-        }
-    }
-
+impl SocksConfig {
     /// reads config from env
     pub fn new() -> Self {
-        let host = Self::get_env("REVPROXY_SOCKS5_HOST", "127.0.0.1".into());
-        let port: u16 = Self::get_env("REVPROXY_SOCKS5_PORT", "1080".into())
+        let host = get_env("REVPROXY_SOCKS_HOST", "127.0.0.1".into());
+        let port: u16 = get_env("REVPROXY_SOCKS_PORT", "1080".into())
             .parse()
             .expect("PORT is not a number");
         let pub_addr =
-            Self::get_env_opt("REVPROXY_PUB_ADDR").map(|x| x.parse().expect("Invalid IP address"));
-        let auth_method = Self::get_env("REVPROXY_AUTH_METHOD", "userpass".into());
+            get_env_opt("REVPROXY_PUB_ADDR").map(|x| x.parse().expect("Invalid IP address"));
+        let auth_method = get_env("REVPROXY_SOCKS_AUTH_METHOD", "userpass".into());
         let auth = match auth_method.as_str() {
             "ldap" => Auth::Ldap,
             "no_auth" => Auth::NoAuth,
             "userpass" => {
-                let username = Self::get_env("REVPROXY_AUTH_USER", "user".into());
-                let password = Self::get_env("REVPROXY_AUTH_PASS", "pass".into());
+                let username = get_env("REVPROXY_SOCKS_AUTH_USER", "user".into());
+                let password = get_env("REVPROXY_SOCKS_AUTH_PASS", "pass".into());
                 Auth::UserPass { username, password }
             }
             _ => panic!(
                 "Invalid auth method \"{auth_method}\": Allowed are `ldap`, `no_auth`, `userpass`."
             ),
         };
-        let timeout: usize = Self::get_env("REVPROXY_TIMEOUT", "10".into())
+        let timeout: usize = get_env("REVPROXY_SOCKS_TIMEOUT", "10".into())
             .parse()
             .expect("TIMEOUT must be an integer");
-        let enable_udp = Self::get_env_bool("REVPROXY_SOCKS5_ENABLE_UDP").unwrap_or(false);
-        let resolve_dns = Self::get_env_bool("REVPROXY_SOCKS5_RESOLVE_DNS").unwrap_or(true);
+        let enable_udp = get_env_bool("REVPROXY_SOCKS_ENABLE_UDP").unwrap_or(false);
+        let resolve_dns = get_env_bool("REVPROXY_SOCKS_RESOLVE_DNS").unwrap_or(true);
 
         Self {
             addr: format!("{host}:{port}"),
@@ -80,5 +58,97 @@ impl Config {
             resolve_dns,
             enable_udp,
         }
+    }
+}
+
+pub struct HttpConfig {
+    pub addr: String,
+    pub https: Option<HttpsConfig>,
+    pub auth: Auth,
+}
+
+impl HttpConfig {
+    /// reads config from env
+    pub fn new() -> Self {
+        let host = get_env("REVPROXY_HTTP_HOST", "127.0.0.1".into());
+        let port: u16 = get_env("REVPROXY_HTTP_PORT", "8080".into())
+            .parse()
+            .expect("PORT is not a number");
+        let auth_method = get_env("REVPROXY_HTTP_AUTH_METHOD", "userpass".into());
+        let auth = match auth_method.as_str() {
+            "ldap" => Auth::Ldap,
+            "no_auth" => Auth::NoAuth,
+            "userpass" => {
+                let username = get_env("REVPROXY_HTTP_AUTH_USER", "user".into());
+                let password = get_env("REVPROXY_HTTP_AUTH_PASS", "pass".into());
+                Auth::UserPass { username, password }
+            }
+            _ => panic!(
+                "Invalid auth method \"{auth_method}\": Allowed are `ldap`, `no_auth`, `userpass`."
+            ),
+        };
+
+        let https = match get_env_bool("REVPROXY_HTTPS") {
+            Some(true) => Some(HttpsConfig::new()),
+            _ => None,
+        };
+
+        Self {
+            addr: format!("{host}:{port}"),
+            https,
+            auth,
+        }
+    }
+}
+
+pub struct HttpsConfig {
+    pub addr: String,
+    pub cert_path: String,
+    pub key_path: String,
+}
+
+impl HttpsConfig {
+    fn new() -> Self {
+        let host = get_env("REVPROXY_HTTPS_HOST", "127.0.0.1".into());
+        let port: u16 = get_env("REVPROXY_HTTPS_PORT", "4430".into())
+            .parse()
+            .expect("PORT is not a number");
+        let cert_path =
+            get_env_opt("REVPROXY_HTTPS_CERT").expect("CERT must be set if HTTPS is enabled");
+        let key_path = get_env_opt("REVPROXY_HTTPS_CERT_KEY")
+            .expect("CERT_KEY must be set if HTTPS is enabled");
+
+        Self {
+            addr: format!("{host}:{port}"),
+            cert_path,
+            key_path,
+        }
+    }
+}
+
+#[inline]
+fn get_env(key: &str, def: String) -> String {
+    match std::env::var(key) {
+        Ok(var) => var,
+        Err(std::env::VarError::NotPresent) => def,
+        Err(_) => panic!("Env var {key} is not unicode"),
+    }
+}
+
+#[inline]
+fn get_env_opt(key: &str) -> Option<String> {
+    match std::env::var(key) {
+        Ok(var) => Some(var),
+        Err(std::env::VarError::NotPresent) => None,
+        Err(_) => panic!("Env var {key} is not unicode"),
+    }
+}
+
+#[inline]
+fn get_env_bool(key: &str) -> Option<bool> {
+    match std::env::var(key) {
+        Ok(var) => Some(&var != "0"),
+        Err(std::env::VarError::NotPresent) => None,
+        Err(_) => panic!("Env var {key} is not unicode"),
     }
 }
