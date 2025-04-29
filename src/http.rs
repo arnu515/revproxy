f2b9aca7b399cc3e85572add867344cb8fa98339
Aarnav Pai <52203828+arnu515@users.noreply.github.com>
2025-04-29 11:32:39 +0530

    feat: use maud

--- src/http.rs
+++ src/http.rs
@@ -1,10 +1,13 @@
-use axum::{Router, response::Html, routing::get};
+use axum::{Router, routing::get};
+use maud::{Markup, html};
 
 pub async fn run_app(listener: tokio::net::TcpListener) {
     let router = Router::new().route("/", get(index));
     axum::serve(listener, router).await.unwrap();
 }
 
-async fn index() -> Html<&'static str> {
-    Html("<h1>Hello, world!</h1>")
+async fn index() -> Markup {
+    html! {
+        h1 { "Hello, world!" }
+    }
 }
